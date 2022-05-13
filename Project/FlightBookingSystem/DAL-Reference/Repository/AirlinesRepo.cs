c73fa7c970bd8b57e1b19fcdeef8ae7181f9197c
DhivyaSriMurugesan04 <dhivyasridharanm@gmail.com>
2022-05-13 20:37:35 +0530

    updated-Code-commit

---
+++ Project/FlightBookingSystem/DAL-Reference/Repository/AirlinesRepo.cs
@@ -0,0 +1,51 @@
+﻿using DAL_Reference.Interfaces;
+using DAL_Reference.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DAL_Reference.Repository
+{
+    public class AirlinesRepo : RepositoryBase<TblAirlines>, IAirlinesRepository
+    {
+        private FlightBookingApplicationDBContext _repositoryContext;
+
+        public AirlinesRepo(FlightBookingApplicationDBContext repositoryContext)
+            : base(repositoryContext) => _repositoryContext = repositoryContext;
+        public IEnumerable<TblAirlines> GetAllAirlnes()
+        {
+            return FindAll()
+               .OrderBy(u => u.AirlineId)
+               .ToList();
+        }
+
+        public TblAirlines GetAirlineById(string airlineID)
+        {
+            return FindByCondition(u => u.AirlineId == airlineID).FirstOrDefault();
+        }
+        
+
+        public IEnumerable<TblAirlines> GetFlightsByAirlineId(string airlineID)
+        {
+            return _repositoryContext.TblAirlines.Include(o => o.TblFlight).Where(o => o.AirlineId == airlineID).ToList();
+            
+        }
+        
+        public void CreateAirline(TblAirlines airlinesMaster)
+        {
+            Create(airlinesMaster);
+        }
+
+        public void UpdateAirline(TblAirlines airlinesMaster)
+        {
+            Update(airlinesMaster);
+        }
+
+        public void DeleteAirline(TblAirlines airlinesMaster)
+        {
+            Delete(airlinesMaster);
+        }
+    }
+}
